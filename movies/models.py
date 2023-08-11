007e1bfde7feef0c728b4a4f91857a05a24779c3
ivan <supreltd@gmail.com>
2023-08-11 16:33:45 +0300

    Model Category

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
 class Category(models.Model):
     """Категории"""
-    name = models
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
